e7fb2db53b4e149a9209a5255f347e0e459b7ff3
Coll-art <collin5050m@Gmail.com>
2025-08-25 13:52:02 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,8 @@
+/* ~~~~~~~~~~~ Exercise 1: Swapping Variables ~~~~~~~~~~~ */
+int a = 1;
+int b = 2;
+Console.WriteLine($"a:{a}b:{b}");
+int temp = a;
+ a = b;
+ b = temp;
+Console.WriteLine($"a:{a}b:{b}");
