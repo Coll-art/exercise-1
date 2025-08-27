2fe66051308fa93532a9b3a081ef622696187525
Coll-art <collin5050m@Gmail.com>
2025-08-27 12:52:12 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ int temp = a;
  a = b;
  b = temp;
 Console.WriteLine($"a:{a}b:{b}");
-var name = Daryle;
+var name = "Daryle";
 var age = 10;
 var hometown = "smithfield";
 string intro = $"{name} is from {hometown} and is {age} years old";
